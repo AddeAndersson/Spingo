311e0acc697e2f186fbbc40275c0d54ae39bd38d
Adrian <adde.a@hotmail.com>
2023-03-11 13:58:31 +0100

    Test commit

---
+++ Assets/DiceScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceScript : MonoBehaviour
+{
+	private: 
+		String[] diceSides = {
+			"KAVQ",
+			"Spingo",
+			"GEJÖ",
+			"DESO",
+			"KIER",
+			"CAHI",
+			"NÄFI",
+			"TXUP",
+			"LYOS",
+			"ZABM",
+			"REDN",
+			"LATÅ"
+		};    
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
